54fd1f894781bd511a6b0ca248116406be30448e
mahmoo30 <wasif.96@gmail.com>
2023-07-25 12:48:32 -0500

    added About page code - initial

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/styles.css";
+
+const About = () => {
+    return  (
+        <div className="about">
+            <h1>About Me</h1>
+            <p>
+            I am a fullstack developer with experience in building scalable, secure and reliable web applications using various frameworks and technologies. 
+            I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and 
+            industry standards. I am always looking for new challenges and opportunities to grow as a developer.
+            </p>
+        </div>
+    );
+};
+
+export default About;
