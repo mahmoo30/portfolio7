c862994baa77bb8eaff70f1ba20831a39a62998a
mahmoo30 <wasif.96@gmail.com>
2023-07-26 17:45:31 -0500

    Added images to projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -11,7 +11,7 @@ const Projects = () => {
                     <div className="project-info">
                         <h2>Park Quest</h2>
                         <div className="project-logo">
-                        <img src="https://i.imgur.com/8X3Q1ZT.png" alt="resume" />
+                        <img src="park.png" alt="park" />
                         </div>
                         <p>
                         Park Quest is a web application that allows users to search for national parks by selecting a state. The application also provides the current weather for each park location, helping users better plan their visits.
@@ -23,7 +23,7 @@ const Projects = () => {
                     <div className="project-info">
                         <h2>Battlehawks Tailgates</h2>
                         <div className="project-logo">
-                        <img src="https://i.imgur.com/8X3Q1ZT.png" alt="resume" />
+                        <img src="battle.png" alt="battle" />
                         </div>
                         <p>
                         Battlehawks Tailgates is a platform designed for people to sign up for tailgates at the Dome for Battlehawks games. It aims to create a community of tailgaters and provide them with a convenient platform to interact, organize, and sign up for various duties related to the tailgating experience.
@@ -35,7 +35,7 @@ const Projects = () => {
                     <div className="project-info">
                         <h2>Workout Customizer</h2>
                         <div className="project-logo">
-                        <img src="https://i.imgur.com/8X3Q1ZT.png" alt="resume" />
+                        <img src="workout.png" alt="workout" />
                         </div>
                         <p>
                         Workout Customizer is an application that allows users to find workouts and save them to their profile for easy access later on. It will assist users while they are working out by having all of their workouts easily accessible on one page with clear instructions on how to safely and effectively perform each exercise.
@@ -47,7 +47,7 @@ const Projects = () => {
                     <div className="project-info">
                         <h2>Book Search</h2>
                         <div className="project-logo">
-                        <img src="https://i.imgur.com/8X3Q1ZT.png" alt="resume" />
+                        <img src="book.png" alt="book" />
                         </div>
                         <p>
                         Book Search is a web application that allows users to search for books by title. The application also provides a brief description of each book and allow users to save them to their profile for easy access later on.
@@ -59,7 +59,7 @@ const Projects = () => {
                     <div className="project-info">
                         <h2>Work Day Planner</h2>
                         <div className="project-logo">
-                        <img src="https://i.imgur.com/8X3Q1ZT.png" alt="resume" />
+                        <img src="plan.png" alt="plan" />
                         </div>
                         <p>
                         Work Day Planner is a simple calendar application that allows users to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.
@@ -71,7 +71,7 @@ const Projects = () => {
                     <div className="project-info">
                         <h2>Weather Dashboard</h2>
                         <div className="project-logo">
-                        <img src="https://i.imgur.com/8X3Q1ZT.png" alt="resume" />
+                        <img src="weather.png" alt="weather" />
                         </div>
                         <p>
                         Weather Dashboard is a web application that allows users to search for the current weather and 5-day forecast for any city. The application uses the OpenWeather API to retrieve weather data for cities and localStorage to store any persistent data.
