df2df9af9fef545a7955b421cef185c6cd21583a
mahmoo30 <wasif.96@gmail.com>
2023-07-26 17:32:31 -0500

    Added icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "../styles/styles.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     return  (
         <footer className="footer enclose problem">
             <div className="footer-container">
                 <div className="footer-item">
-                    <a href="/">Github</a>
+                    <a href="https://github.com/mahmoo30">
+                    <FaGithub size={32} />Github</a>
                 </div> 
                 <div className="footer-item">
-                    <a href="/">LinkedIn</a>
-                </div>
-                <div className="footer-item">
-                    <a href="/">Email</a>
+                    <a href="https://www.linkedin.com/in/wasifuddin-mahmood/">
+                    <FaLinkedin size={32} />LinkedIn</a>
                 </div>
             </div>
         </footer>
--- src/pages/About.js
+++ src/pages/About.js
@@ -11,12 +11,12 @@ const About = () => {
                 Hi there! My name is Wasif Mahmood, I am originally from Bangladesh but I now live in St. Louis, MO.
             </p>
             <p>
-            I am a fullstack developer with experience in building scalable, secure and reliable web applications using various frameworks and technologies. 
-            I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and 
-            industry standards. I am always looking for new challenges and opportunities to grow as a developer.
+                I am a fullstack developer with experience in building scalable, secure and reliable web applications using various frameworks and technologies. 
+                I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and 
+                industry standards. I am always looking for new challenges and opportunities to grow as a developer.
             </p>
             <p>
-            I've worked on a variety of projects and I'm proud of the progress I've made so far. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.
+                I've worked on a variety of projects and I'm proud of the progress I've made so far. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.
             </p>
         </center>
         </div>
